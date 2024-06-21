0beb2d5374def4d74a09805c070b035fdc8a1176
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2024-06-21 17:35:37 -0300

    Added relevant functions to return LaraMoney

--- src/LaraMoney.php
+++ src/LaraMoney.php
@@ -7,6 +7,67 @@ use Money\Currency;
 use Money\Money;
 
 class LaraMoney extends Money implements Wireable{
+
+    public function __construct(int|string $amount, Currency $currency)
+    {
+            parent::__construct($amount, $currency);
+    }
+
+    public function add(LaraMoney ...$addends): LaraMoney
+    {
+        return parent::add(...$addends);
+    }
+
+    public function subtract(LaraMoney ...$subtrahends): LaraMoney
+    {
+        return parent::subtract(...$subtrahends);
+    }
+
+    public function multiply(int|string $multiplier, int $roundingMode = self::ROUND_HALF_UP): LaraMoney
+    {
+        return parent::multiply($multiplier, $roundingMode);
+    }
+
+    public function divide(int|string $divisor, int $roundingMode = self::ROUND_HALF_UP): LaraMoney
+    {
+        return parent::divide($divisor, $roundingMode);
+    }
+
+    public function mod(LaraMoney|int|string $divisor): LaraMoney
+    {
+        return parent::mod($divisor);
+    }
+
+    public function absolute(): LaraMoney
+    {
+        return parent::absolute();
+    }
+
+    public function negative(): LaraMoney
+    {
+        return parent::negative();
+    }
+
+    public function min(LaraMoney $first, LaraMoney ...$collection): LaraMoney
+    {
+        return parent::min($first, ...$collection);
+    }
+
+    public function max(LaraMoney $first, LaraMoney ...$collection): LaraMoney
+    {
+        return parent::max($first, ...$collection);
+    }
+
+    public function sum(LaraMoney $first, LaraMoney ...$collection): LaraMoney
+    {
+        return parent::sum($first, ...$collection);
+    }
+
+    public function avg(LaraMoney $first, LaraMoney ...$collection): LaraMoney
+    {
+        return parent::avg($first, ...$collection);
+    }
+    
     public function __toString()
     {
         return LaraMoneyHelper::moneyToString($this);
