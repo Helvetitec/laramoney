e2978af0c70523f034d0f2f3fbae1e2681c0660f
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2024-07-13 18:14:57 -0300

    Fixed exception if returned value is NULL

--- src/Casts/LaraMoneyCast.php
+++ src/Casts/LaraMoneyCast.php
@@ -18,6 +18,9 @@ class LaraMoneyCast implements CastsAttributes
      */
     public function get(Model $model, string $key, mixed $value, array $attributes): Money
     {
+        if(is_null($value)){
+            return config('laramoney.casts_null', false) ? null : LaraMoneyHelper::createMoney(0, config('laramoney.default_currency', 'BRL'));
+        }
         $json = json_decode($value, true);
         return new Money($json["amount"], new Currency($json["currency"]));
     }
