5f189ebea86d8a1750cbb87e72ce21f3b2517e0c
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2024-07-13 18:14:23 -0300

    Added casts_null config variable

--- config/config.php
+++ config/config.php
@@ -3,4 +3,5 @@
 return [
     'default_currency' => 'BRL',
     'values_in_cents' => true, //If set to false, values will be saved with * 100
+    'casts_null' => false //If true, the LaraMoneyCast can return NULL, if not it will return 0 with default_currency
 ];
