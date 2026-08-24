235ab8be0b1104eabb66281dbdab8e479b29d5e3
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2026-08-24 09:25:53 -0300

    fix: Updated Casts to support Money facade and not old helper class

--- src/Casts/LaraMoneyCast.php
+++ src/Casts/LaraMoneyCast.php
@@ -5,7 +5,7 @@ namespace LaraMoney\Casts;
 use Exception;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Database\Eloquent\Model;
-use LaraMoney\LaraMoneyHelper;
+use LaraMoney\Facades\Money as FacadesMoney;
 use Money\Currency;
 use Money\Money;
 
@@ -19,7 +19,7 @@ class LaraMoneyCast implements CastsAttributes
     public function get(Model $model, string $key, mixed $value, array $attributes): ?Money
     {
         if(is_null($value) || trim($value) == ''){
-            return config('laramoney.casts_null', false) ? null : LaraMoneyHelper::createMoney(0, config('laramoney.default_currency', 'BRL'));
+            return config('laramoney.casts_null', false) ? null : FacadesMoney::make(0, config('laramoney.default_currency', 'BRL'));
         }
         $json = json_decode($value, true);
         return new Money($json["amount"], new Currency($json["currency"]));
@@ -36,10 +36,10 @@ class LaraMoneyCast implements CastsAttributes
             return null;
         }
         if(is_array($value)){
-            $value = LaraMoneyHelper::createMoney($value["amount"] * (config('laramoney.values_in_cents', false) ? 1 : 100), $value["currency"]);
+            $value = FacadesMoney::make($value["amount"] * (config('laramoney.values_in_cents', false) ? 1 : 100), $value["currency"]);
         }
         if(is_numeric($value)){
-            $value = LaraMoneyHelper::createMoney($value * (config('laramoney.values_in_cents', false) ? 1 : 100), config('laramoney.default_currency', 'BRL'));
+            $value = FacadesMoney::make($value * (config('laramoney.values_in_cents', false) ? 1 : 100), config('laramoney.default_currency', 'BRL'));
         }
         if(!$value instanceof Money){
             throw new Exception("Value is not an instance of Money\Money. => ".$value);
