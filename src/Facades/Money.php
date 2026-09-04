f04d79da1cc82ce93b8d0f4177e5a4badeefb11d
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2026-09-04 14:19:57 -0300

    fix: Fixed invalid phpdocs in Money facade for param types

--- src/Facades/Money.php
+++ src/Facades/Money.php
@@ -8,14 +8,14 @@ use Money\Money as MoneyMoney;
 use Money\Currency;
 
 /**
- * @method static string format(?Money $money, ?string $locale = null, bool $withSign = false, bool $allowNull = true)
+ * @method static string format(?MoneyMoney $money, ?string $locale = null, bool $withSign = false, bool $allowNull = true)
  * @method static string formatCents(int $valueInCents, string|Currency $currency = "BRL", ?string $locale = null, bool $withSign = false)
  * @method static MoneyMoney zero(string|Currency $currency = "BRL")
  * @method static MoneyMoney make(string|int|null $valueInCents = null, string|Currency $currency = "BRL")
- * @method static string toJson(Money $money)
- * @method static MoneyMoney parse(Money|string|array $value, bool $convertNull = false)
- * @method static MoneyMoney getPercentage(Money $value, int $percentage)
- * @method static MoneyMoney difference(Money $value1, Money $value2, bool $absolute = false)
+ * @method static string toJson(MoneyMoney $money)
+ * @method static MoneyMoney parse(MoneyMoney|string|array $value, bool $convertNull = false)
+ * @method static MoneyMoney getPercentage(MoneyMoney $value, int $percentage)
+ * @method static MoneyMoney difference(MoneyMoney $value1, MoneyMoney $value2, bool $absolute = false)
  * 
  * @see LaraMoney
  */
