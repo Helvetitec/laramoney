c319724c9537fdba25bbe3ea80703694f2b1f013
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2026-09-04 11:23:41 -0300

    fix: Added intellisense support for Money facade

--- src/Facades/Money.php
+++ src/Facades/Money.php
@@ -3,7 +3,21 @@
 namespace LaraMoney\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use LaraMoney\LaraMoney;
+use Money\Money as MoneyMoney;
+use Money\Currency;
 
+/**
+ * @method static string format(?Money $money, ?string $locale = null, bool $withSign = false, bool $allowNull = true)
+ * @method static string formatCents(int $valueInCents, string|Currency $currency = "BRL", ?string $locale = null, bool $withSign = false)
+ * @method static MoneyMoney zero(string|Currency $currency = "BRL")
+ * @method static MoneyMoney make(string|int|null $valueInCents = null, string|Currency $currency = "BRL")
+ * @method static string toJson(Money $money)
+ * @method static MoneyMoney parse(Money|string|array $value, bool $convertNull = false)
+ * @method static MoneyMoney getPercentage(Money $value, int $percentage)
+ * 
+ * @see LaraMoney
+ */
 class Money extends Facade
 {
     protected static function getFacadeAccessor(): string
